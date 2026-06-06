f3b87b5f7dd998cdf083aefe688b088adadb8e95
Jonathan Hernandez <166683841+jona2022-02@users.noreply.github.com>
2026-06-05 23:03:05 -0600

    descargar nueva  app mobile

--- verificador-dte/lib/mobile-app.ts
+++ verificador-dte/lib/mobile-app.ts
@@ -1,5 +1,5 @@
 export const mobileAppDownloadUrl =
-  'https://firebasestorage.googleapis.com/v0/b/kaydte-48e8a.firebasestorage.app/o/apk%2Fapplication-0af9fda5-d65b-4692-98b3-4edc93a63a9a.apk?alt=media';
+  'https://firebasestorage.googleapis.com/v0/b/kaydte-48e8a.firebasestorage.app/o/apk%2Fapplication-59f479c6-f068-40ee-b527-025a227d27be.apk?alt=media';
 
 export const mobileAppQrUrl =
   'https://verificadordtev2.cuscadev.com/api/mobile-app/download';
